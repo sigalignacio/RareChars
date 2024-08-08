ffef95db44e9919ec765c9a711755ebcfa9b0205
Ignacio <ignaciosigal04@gmail.com>
2024-08-08 16:32:16 -0300

    run build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rareChars/',  // esto asegura que las rutas se resuelvan bien en GitHub Pages
   plugins: [react()],
 })
