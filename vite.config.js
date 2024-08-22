f2691282e1190661a30da81fae89d1676be2af0c
Ignacio <ignaciosigal04@gmail.com>
2024-08-22 00:29:48 -0300

    Update homepage URL

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/rareChars/',  // esto asegura que las rutas se resuelvan bien en GitHub Pages
   plugins: [react()],
 })
